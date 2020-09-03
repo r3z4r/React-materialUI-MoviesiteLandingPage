d453637b99adf0f66d4d64d334b33a74da358089
MohamadReza Rohani <MohamadReza.Rohani@tecnotree.com>
2020-09-04 01:00:48 +0430

    befor adding funzone branch

--- src/Containers/Layout/Layout.js
+++ src/Containers/Layout/Layout.js
@@ -1,22 +1,22 @@
-import React from 'react';
+import React from 'react'
 import Grid from '@material-ui/core/Grid'
 import Header from '../Header/Header'
 
-const Layout = (props) => {
+const Layout = props => {
   return (
-    <Grid container direction='coloumn' xs={12} >
+    <Grid container direction='coloumn' xs={12}>
       <Grid xs={12} item>
-        <Header/>
+        <Header />
       </Grid>
       <Grid item container>
-        <Grid item xs={0} sm={2}></Grid>
+        <Grid item xs={false} sm={2} />
         <Grid item xs={12} sm={8}>
           {props.children}
         </Grid>
-        <Grid item xs={0} sm={2}></Grid>
+        <Grid item xs={false} sm={2} />
       </Grid>
     </Grid>
-  );
+  )
 }
 
-export default Layout;
+export default Layout

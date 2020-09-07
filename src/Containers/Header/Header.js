0e94dea8c3111d25218e206d8d0c1bef586a0c49
MohamadReza Rohani <MohamadReza.Rohani@tecnotree.com>
2020-09-07 19:24:07 +0430

    adding pagination

--- src/Containers/Header/Header.js
+++ src/Containers/Header/Header.js
@@ -1,15 +1,15 @@
-import React from 'react'
-import { AppBar, Toolbar } from '@material-ui/core'
-import FirstToolbar from '../FirstToolbar/FirstToolbar'
-import MenuBar from '../Menubar/Menubar'
+import React from "react";
+import {AppBar, Toolbar} from "@material-ui/core";
+import FirstToolbar from "../FirstToolbar/FirstToolbar";
+import MenuBar from "../Menubar/Menubar";
 
-export default function Header (props) {
-  return (
-    <AppBar position='static'>
-      <FirstToolbar handleDrawerToggle={props.handleDrawerToggle} />
-      <Toolbar>
-        <MenuBar />
-      </Toolbar>
-    </AppBar>
-  )
+export default function Header(props) {
+	return (
+		<AppBar position="sticky">
+			<FirstToolbar handleDrawerToggle={props.handleDrawerToggle} />
+			<Toolbar>
+				<MenuBar />
+			</Toolbar>
+		</AppBar>
+	);
 }
